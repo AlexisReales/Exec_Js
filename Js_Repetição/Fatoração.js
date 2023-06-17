1e460e841413faa6217bc95235da97d10005d6b1
Reales <alexisrealesb@gmail.com>
2023-06-16 22:54:51 -0300

    Last update

---
+++ Js_Repetição/Fatoração.js
@@ -0,0 +1,11 @@
+function fator(){
+    let num = 3
+    for(i = num - 1; i >= 1; i--){
+        num *= i
+    }
+    console.log(num)
+}
+
+fator()
+
+//Mostrar o Fator de um número no qual o usuário colocar em 'num'
